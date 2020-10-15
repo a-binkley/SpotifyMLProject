8da9e1c4d8e72e40cfa2dc4455925da112f87d2e
adria <adria@10.0.0.46>
2020-10-15 12:31:42 -0400

    Created initial class for BingoGenerator, have basic parts of Person

---
+++ 591 Recitation Code/src/BingoGenerator.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class BingoGenerator {
+
+	public static void main(String[] args) {
+		ArrayList<Integer> bingo_spots = new ArrayList<Integer>();
+		for (int i = 0; i < 25; i++) {
+			bingo_spots.add(i, i+1);
+		}
+		Collections.shuffle(bingo_spots);
+		System.out.println(bingo_spots.toString());
+	}
+
+}
