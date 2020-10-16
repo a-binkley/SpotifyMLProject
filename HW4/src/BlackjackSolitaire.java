386403f8c82dfcc489b7a5d9799e376095e09068
adria <adria@10.0.0.46>
2020-10-15 23:28:45 -0400

    Debugged display formatting
    Still contains updateRowValue() and updateColValue()

--- HW4/src/BlackjackSolitaire.java
+++ HW4/src/BlackjackSolitaire.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class BlackjackSolitaire {
 	public static Card[] board;
 	public static int[] column_points = {0, 0, 0, 0, 0};
@@ -14,25 +16,23 @@ public class BlackjackSolitaire {
 	public void displayBoard() {
 		//The top two rows will be drawn identically
 		for (int i = 0; i < 2; i++) {
+			System.out.print("        ");
 			//There are 7 slots total per row, including cards and empty spaces
-			for (int j = 0; j < 7; j++) {
-				if (j < 2) {
-					System.out.print("   ");
-				} else {
-					int current_space = i*5 + (j-2);
-					String padding_space = "   ";
-					if (board[current_space] != null) {
-						// Display the given card value
-						for (int k = 0; k < board[current_space].value.length(); k++) {
-							padding_space = padding_space.substring(1);
-						}
-					} else {
-						// Display the space number
-						for (int k = 0; k < ("" + (current_space+1)).length(); k++) {
-							padding_space = padding_space.substring(1);
-						}
+			for (int j = 1; j < 6; j++) {
+				int current_space = i*5 + j;
+				String padding_space = "   ";
+				if (board[current_space-1] != null) {
+					// Display the given card value
+					for (int k = 0; k < board[current_space-1].value.length(); k++) {
+						padding_space = padding_space.substring(1);
 					}
-					System.out.print(padding_space + (current_space+1));
+					System.out.print(padding_space + board[current_space-1].value);
+				} else {
+					// Display the space number
+					for (int k = 0; k < ("" + current_space).length(); k++) {
+						padding_space = padding_space.substring(1);
+					}
+					System.out.print(padding_space + current_space);
 				}
 				System.out.print(" ");
 			}
@@ -45,36 +45,38 @@ public class BlackjackSolitaire {
 					System.out.print("   ");
 				} else if (j > 2) {
 					//Display the bottom two rows
-					int current_space = 10 + (i*3) + j-3;
+					int current_space = 11 + (i*3) + j-3;
 					String padding_space = "   ";
-					if (board[current_space] != null) {
+					if (board[current_space-1] != null) {
 						// Display the given card value
-						for (int k = 0; k < board[current_space].value.length(); k++) {
+						for (int k = 0; k < board[current_space-1].value.length(); k++) {
 							padding_space = padding_space.substring(1);
 						}
+						System.out.print(padding_space + board[current_space-1]);
 					} else {
 						// Display the space number
 						for (int k = 0; k < ("" + current_space).length(); k++) {
 							padding_space = padding_space.substring(1);
 						}
+						System.out.print(padding_space + current_space);
 					}
-					System.out.print(padding_space + (current_space+1));
 				} else {
 					//Display the discard square
 					int current_space = 16 + (i*2) + j;
 					String padding_space = "   ";
-					if (board[current_space] != null) {
+					if (board[current_space-1] != null) {
 						// Display the given card value
-						for (int k = 0; k < board[current_space].value.length(); k++) {
+						for (int k = 0; k < board[current_space-1].value.length(); k++) {
 							padding_space = padding_space.substring(1);
 						}
+						System.out.print(padding_space + board[current_space-1]);
 					} else {
 						// Display the space number
 						for (int k = 0; k < ("" + current_space).length(); k++) {
 							padding_space = padding_space.substring(1);
 						}
+						System.out.print(padding_space + current_space);
 					}
-					System.out.print(padding_space + (current_space+1));
 				}
 				System.out.print(" ");
 			}
@@ -86,6 +88,7 @@ public class BlackjackSolitaire {
 	 * Adds the point value of the input Card to the corresponding row sum
 	 */
 	public void updateRowValue(Card c, int location) {
+		// TODO: write logic for Blackjack combos, busting, etc.
 		if (location < 6) {
 			// First row
 			row_points[0] += c.points;
@@ -108,6 +111,7 @@ public class BlackjackSolitaire {
 	 * Adds the point value of the input Card to the corresponding column sum
 	 */
 	public void updateColValue(Card c, int location) {
+		// TODO: write logic for Blackjack combos, busting, etc.
 		if (location == 1 || location == 6) {
 			// First column
 			column_points[0] += c.points;
@@ -130,17 +134,34 @@ public class BlackjackSolitaire {
 	/*
 	 * Allows the player to pick where they want the current top card to be placed on the board
 	 */
-	public void placeCard(Card c, int location) {
+	public boolean placeCard(Card c, int location) {
 		// Check if the chosen target location is invalid
 		if (board[location-1] != null) {
 			System.out.println("ERROR: Cannot place card in already full spot.");
 			//call to repeat prompt method
-			return;
+			return false;
 		}
-		// If the target is valid, place the card there and update the board_filled and point arrays
+		// If the target is valid, place the card there and update board_filled and point arrays
 		board[location-1] = c;
 		updateRowValue(c, location);
 		updateColValue(c, location);
+		return true;
+	}
+	
+	/*
+	 * Calculate the final score for the game
+	 */
+	public int calculateScore() {
+		int score_sum = 0;
+		for (int i = 0; i < 5; i++) {
+			if (i < 4) {
+				score_sum += row_points[i] + column_points[i];
+			} else {
+				// row_points[4] would be out-of-bounds
+				score_sum += column_points[i];
+			}
+		}
+		return score_sum;
 	}
 	
 	/* 
@@ -151,10 +172,38 @@ public class BlackjackSolitaire {
 		displayBoard();
 		Deck gameDeck = new Deck();
 		gameDeck.shuffle();
-		//TEMP
-		for (int i = 0; i < 52; i++) {
-			System.out.print(gameDeck.drawCard().value + " ");
+		
+		// Game round loop setup
+		boolean isOver = false;
+		boolean hasNull = false;
+		Scanner s = new Scanner(System.in);
+		Card currentCard = gameDeck.drawCard();
+		
+		// Game round loop
+		while (!isOver) {
+			System.out.println("Enter the target spot to place the card (" + currentCard.value + "):");
+			int target = s.nextInt();
+			if (!placeCard(currentCard, target)) {
+				// If the card placement is invalid, re-display the prompt
+				continue; 
+			}
+			displayBoard();
+			currentCard = gameDeck.drawCard();
+			
+			// Check the state of the board to see if we're done
+			hasNull = false;
+			for (int i = 0; i < 16; i++) {
+				if (board[i] == null) {
+					hasNull = true;
+					break;
+				}
+			}
+			if (!hasNull) {
+				isOver = true; // The game is over, so total the score and show it in console
+			}
 		}
 		
+		// The game is over and score will be calculated
+		System.out.println("Game over! Final score: " + calculateScore());
 	}
 }
