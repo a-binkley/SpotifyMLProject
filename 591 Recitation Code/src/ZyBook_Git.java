cacf0404d588d362f9cb29fa41dab0b92f639f9b
adria <adria@10.0.0.46>
2020-10-15 21:36:43 -0400

    Removed getters/setters

--- 591 Recitation Code/src/ZyBook_Git.java
+++ 591 Recitation Code/src/ZyBook_Git.java
@@ -3,33 +3,6 @@ public class ZyBook_Git {
 	private int itemPrice; //Initialized in default constructor to 0
 	private int itemQuantity; //Initialized in default constructor to 0
 	
-	
-	
-	public String getName() {
-		return itemName;
-	}
-
-	public void setName(String itemName) {
-		this.itemName = itemName;
-	}
-
-	public int getPrice() {
-		return itemPrice;
-	}
-
-	public void setPrice(int itemPrice) {
-		this.itemPrice = itemPrice;
-	}
-
-	public int getQuantity() {
-		return itemQuantity;
-	}
-
-	public void setQuantity(int itemQuantity) {
-		this.itemQuantity = itemQuantity;
-	}
-
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
