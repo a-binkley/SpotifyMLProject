397057bae6d13ddd5e22beb4d5ffd4a9cb8306ab
adria <adria@10.0.0.46>
2020-10-15 21:33:26 -0400

    Added private variables

---
+++ 591 Recitation Code/src/ZyBook_Git.java
@@ -0,0 +1,12 @@
+public class ZyBook_Git {
+	private String itemName; //Initialized in default constructor to "none"
+	private int itemPrice; //Initialized in default constructor to 0
+	private int itemQuantity; //Initialized in default constructor to 0
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
